40efd8c9a8def42b60d688014887c8c8234c6ede
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-08-31 19:01:44 -0500

    data type examples n tests

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,15 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 7;
+    auto dec = 1.9;
+    auto flag = true;
+    auto letter = 'a';
+
+    cout<<num<<" "<<dec<<" "<<flag<<" "<<letter;
 
     return 0;
 }
