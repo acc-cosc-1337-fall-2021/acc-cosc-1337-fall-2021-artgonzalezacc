3f90339907c63b6ab017f907c826a5bf153629f5
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-09-17 14:40:21 -0500

    value n reference func params

---
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -0,0 +1,6 @@
+void call_by_val_ref(int num, int& num1, const int& num2)
+{
+    num = 10;
+    num1 = 20;
+    //num2 = 0;
+}
--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "val_ref.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test value(copy) parameter")
+{
+	int num = 5;
+	int num1 = 10;
+	call_by_val_ref(num, num1, num);
+	
+	REQUIRE(num == 5);
+	REQUIRE(num1 == 20);
 }
