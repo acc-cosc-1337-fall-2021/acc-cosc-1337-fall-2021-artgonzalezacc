9585803083c10bb1107179b9fb85156481aa5f7f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2021-09-28 23:51:03 +0000

    Bank account, public, private, constructor

--- src/examples/08_classes/01_bank/bank_account.cpp
+++ src/examples/08_classes/01_bank/bank_account.cpp
@@ -1,1 +1,7 @@
 //bank_account.cpp
+#include "bank_account.h"
+
+int Account::get_balance()
+{
+    return balance;
+}
--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,1 +1,10 @@
 //bank_account.h
+class Account
+{
+public:
+    Account(int b) : balance(b){}
+    int get_balance();    
+    
+private:
+    int balance{0};//iniitalize to zero
+};
