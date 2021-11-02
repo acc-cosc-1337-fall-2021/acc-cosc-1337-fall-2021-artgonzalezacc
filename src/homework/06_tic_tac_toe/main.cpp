33b3e271360dcacc3a31c2a7e95555392f5c3f8f
Art Gonzalez <arturo.gonzalez@austincc.edu>
2021-11-02 23:58:33 +0000

    tic tac toe 1: class, public private func, tests

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,32 @@
+#include "tic_tac_toe.h"
+
+using std::cin;  using std::cout; using std::string;
 
 int main() 
 {
+	string first_player;
+	char choice;
+	TicTacToe game;
+
+	do
+	{
+		cout<<"Enter first player: ";
+		cin>>first_player;
+		game.start_game(first_player);
+		int position;
+
+		while(game.game_over() == false)
+		{
+			cout<<"Enter a position from 1-9: ";
+			cin>>position;
+			game.mark_board(position);
+			game.display_board();
+		}
+
+		cout<<"Do you want to quit? enter N";
+		cin>>choice;
+	}
+	while(choice == 'N');
 
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,67 @@
 //cpp
+#include "tic_tac_toe.h"
+
+using std::cout;
+
+bool TicTacToe::game_over()
+{
+    return check_board_full();
+}
+
+void TicTacToe::start_game(std::string first_player)
+{
+    player = first_player;
+    clear_board();
+}
+
+void TicTacToe::mark_board(int position)
+{
+    pegs[position-1] = player;
+    set_next_player();
+}
+
+std::string TicTacToe::get_player()const
+{
+    return player;
+}
+
+void TicTacToe::display_board() const
+{
+    for(std::size_t i=0; i < pegs.size(); i+=3)
+    {
+        cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
+    }
+}
+
+void TicTacToe::set_next_player()
+{
+    if(player == "X")
+    {
+        player = "O";
+    }
+    else
+    {
+        player =  "X";
+    }
+}
+
+bool TicTacToe::check_board_full()
+{
+    for(std::size_t i=0; i < pegs.size(); i++)
+    {
+        if(pegs[i] == " ")
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void TicTacToe::clear_board()
+{
+    for(auto& peg: pegs)
+    {
+        peg = " ";
+    }
+}
