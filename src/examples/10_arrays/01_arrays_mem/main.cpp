508504a235fa3108ec32320657de0c27bb6158f6
Art Gonzalez <arturo.gonzalez@austincc.edu>
2021-11-04 23:54:40 +0000

    stack arrays: init, access, modify, loop, 2d array

--- src/examples/10_arrays/01_arrays_mem/main.cpp
+++ src/examples/10_arrays/01_arrays_mem/main.cpp
@@ -1,9 +1,20 @@
-
+#include "arrays_mem.h"
 
 int main() 
-{
+{	
+	//stack_array();
+	//array_months();
+	//arrays_and_pointers();
 
-	
+	/*const int SIZE = 3;
+    int month_numbers[SIZE] {1,2,3};
+
+	display_array(month_numbers, SIZE);*/
+
+	int times_table[ROWS][COLS];
+
+	populate_times_table(times_table, ROWS);
+	display_times_table(times_table, ROWS);
 
 	return 0;
 
