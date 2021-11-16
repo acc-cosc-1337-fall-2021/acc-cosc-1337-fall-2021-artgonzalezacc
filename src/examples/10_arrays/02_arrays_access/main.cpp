d2a52c8c97cbb3a1687cfc9bec13522e185a2ece
Art Gonzalez <arturo.gonzalez@austincc.edu>
2021-11-16 23:51:15 +0000

    Array_List in main

--- src/examples/10_arrays/02_arrays_access/main.cpp
+++ src/examples/10_arrays/02_arrays_access/main.cpp
@@ -1,3 +1,22 @@
+#include "arrays_access.h"
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    Array_List list;
+
+    list.add(10);
+    list.add(5);
+    list.add(100);
+
+    std::vector<int> nums = list.to_vector();
+
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+    }
+
+    return 0;
 }
